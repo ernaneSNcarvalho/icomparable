4e99f92905083abf5ad0ce652d15b53fd4c0650d
ernane <ernanecarvalho89@gmail.com>
2021-07-31 10:10:50 -0300

    Finish lecture IComparable

---
+++ InterfaceIcomparable/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using InterfaceIcomparable.Entities;
+
+namespace InterfaceIcomparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\dev\in.txt";
+            try
+            {
+                using(StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach(Employee emp in list)
+                    {
+                        Console.WriteLine(emp);
+                    }
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error ocurred!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
